da6c8195a989f8decc67dca741fd18bbb3a2976a
“noarrassam” <noor.s.rassam@gmail.com>
2021-05-17 14:21:17 -0400

    Add project files.

---
+++ N01389325Project/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace N01389325Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSrch = new System.Windows.Forms.Button();
+            this.JcSelect = new System.Windows.Forms.ComboBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtSrch = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.Label();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.btnExport = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSrch
+            // 
+            this.btnSrch.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSrch.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSrch.Location = new System.Drawing.Point(572, 115);
+            this.btnSrch.Name = "btnSrch";
+            this.btnSrch.Size = new System.Drawing.Size(142, 47);
+            this.btnSrch.TabIndex = 0;
+            this.btnSrch.Text = "Search";
+            this.btnSrch.UseVisualStyleBackColor = true;
+            this.btnSrch.Click += new System.EventHandler(this.btnSrch_Click);
+            // 
+            // JcSelect
+            // 
+            this.JcSelect.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.JcSelect.FormattingEnabled = true;
+            this.JcSelect.Items.AddRange(new object[] {
+            "ID",
+            "Title",
+            "Price"});
+            this.JcSelect.Location = new System.Drawing.Point(349, 120);
+            this.JcSelect.Name = "JcSelect";
+            this.JcSelect.Size = new System.Drawing.Size(198, 39);
+            this.JcSelect.TabIndex = 1;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitle.Location = new System.Drawing.Point(481, 29);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(198, 39);
+            this.txtTitle.TabIndex = 2;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(904, 113);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(142, 49);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(739, 114);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(142, 49);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(397, 27);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(78, 31);
+            this.lblTitle.TabIndex = 5;
+            this.lblTitle.Text = "Title:";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.Location = new System.Drawing.Point(62, 29);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(53, 31);
+            this.lblID.TabIndex = 6;
+            this.lblID.Text = "ID:";
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(123, 29);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(198, 39);
+            this.txtID.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(775, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 31);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Price:";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrice.Location = new System.Drawing.Point(865, 26);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(198, 39);
+            this.txtPrice.TabIndex = 9;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(248, 286);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(767, 414);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // txtSrch
+            // 
+            this.txtSrch.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSrch.Location = new System.Drawing.Point(123, 120);
+            this.txtSrch.Name = "txtSrch";
+            this.txtSrch.Size = new System.Drawing.Size(198, 39);
+            this.txtSrch.TabIndex = 11;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.AutoSize = true;
+            this.txtSearch.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(14, 120);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(103, 31);
+            this.txtSearch.TabIndex = 12;
+            this.txtSearch.Text = "Search:";
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.DataSource = typeof(N01389325Project.Book);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExport.Location = new System.Drawing.Point(514, 217);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(224, 51);
+            this.btnExport.TabIndex = 13;
+            this.btnExport.Text = "SaveToTextFile";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click_1);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Text Files (*.txt)|*.txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(1271, 742);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtSrch);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.JcSelect);
+            this.Controls.Add(this.btnSrch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSrch;
+        private System.Windows.Forms.ComboBox JcSelect;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.TextBox txtSrch;
+        private System.Windows.Forms.Label txtSearch;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ N01389325Project/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace N01389325Project
+{
+    public partial class Form1 : Form
+    {
+        string connString = @"Data Source=DESKTOP-96046EQ\SQLEXPRESS;Initial Catalog=MMABOOKS;User ID=NoorRassam;Password=Humber;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        SqlDataAdapter dataAdapter; //this object here allows us to build the connection between the program and the database
+        DataTable table; //table to hold the information so we can fill the datagrid view
+        SqlConnection conn; //Declares a varialbles to hold sql Code
+        String selectionStatement = "select * from Books"; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            JcSelect.SelectedIndex = 0; //Select First Item when the form loads.
+            dataGridView1.DataSource = bindingSource1;
+
+            //Select all data from Customers Class class
+            GetData("Select * from Books");
+        }
+
+        private void GetData(string selectCommand)
+        {
+            try
+            {
+                dataAdapter = new SqlDataAdapter(selectCommand, connString); //pass in the select command and the connection string
+                table = new DataTable(); //make a new data table object
+                table.Locale = System.Globalization.CultureInfo.InvariantCulture;
+                dataAdapter.Fill(table); //fill the data table 
+                bindingSource1.DataSource = table; //set the data source on the binding source to the table
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);//show useful message to the user of the program
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            SqlCommand command; //declares a new sql command object
+                                //field names in the table
+            string insert = @"insert into Books(ID, Title, Price) 
+                                    values(@ID, @Title, @Price)"; // Parameters names
+
+            // using allows disposing low level resources 
+            using (conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open(); //open the connection 
+                    command = new SqlCommand(insert, conn); // Create new sql command object
+                    command.Parameters.AddWithValue(@"ID", txtID.Text); //Read value from fields and add it to table
+                    command.Parameters.AddWithValue(@"Title", txtTitle.Text);
+                    command.Parameters.AddWithValue(@"Price", txtPrice.Text);
+                    command.ExecuteNonQuery(); //push stuff into the table
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message); //if there is something wrong show user a message.
+                }
+            }
+            GetData("Select * from Books");
+            dataGridView1.Update(); // redraws the data grid view so the new record is visible on the bottom
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            //grab a reference to the current row
+            DataGridViewRow row = dataGridView1.CurrentCell.OwningRow;
+            //grab the value from the ID of the selected record
+            string value = row.Cells["ID"].Value.ToString();
+            //grab the value from the Title field.
+            string title = row.Cells["Title"].Value.ToString();
+            //grab the value from the Price field
+            string price = row.Cells["Price"].Value.ToString();
+            DialogResult result = MessageBox.Show("Do you really want to delete" + txtTitle + " " + ", record " + value, "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            // this is the sql to delete the records from the sql table
+            string deleteStatement = @"Delete from Books where Title = '" + title + "'";
+
+            //check weather ther user really wants to delete records
+            if (result == DialogResult.Yes)
+            {
+                using (conn = new SqlConnection(connString))
+                {
+                    try
+                    {
+                        conn.Open(); //try to open a connection 
+                        SqlCommand comm = new SqlCommand(deleteStatement, conn);
+                        comm.ExecuteNonQuery(); // this line accually causes the deletetion to run
+                        GetData(selectionStatement); // get the data again
+                        dataGridView1.Update(); //redraw the data grid vie with updated information
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message); //runs when something is wring with the connection
+                    }
+                }
+            }
+        }
+
+        private void btnSrch_Click(object sender, EventArgs e)
+        {
+            switch(JcSelect.SelectedItem.ToString())
+            {
+                case "ID":
+                    GetData("select * from Books where lower(ID) like '%" + txtSrch.Text.ToLower() + "%'");
+                    break;
+                case "Title":
+                    GetData("select * from Books where lower(Title) like '%"+ txtSrch.Text.ToLower()+"%'");
+                    break;
+                case "Price":
+                    GetData("select * from Books where lower(Price) like '%" + txtSrch.Text.ToLower() + "%'");
+                    break;
+            }
+        }
+
+        private void btnExport_Click_1(object sender, EventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog()==DialogResult.OK) // Checks whether a user has clicked "OK" button 
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName))
+                {
+                    foreach(DataGridViewRow row in dataGridView1.Rows) // Grab each row in the data grid view
+                    {
+                        foreach(DataGridViewCell cell in row.Cells) // Grabed row, go through the cells of that row
+                        {
+                            sw.Write(cell.Value); // This line writes the value into a text file.
+                            sw.Write("        "); // Tab
+                        }
+                        sw.WriteLine(); //This will push the cursor to the next line.
+                    }
+                    Process.Start("notepad.exe", saveFileDialog1.FileName); //open file in notepad after the file is written to the drive.
+                }
+            }
+        }
+    }
+}
